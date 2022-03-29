a67aab7260358f0b088f34d0fa68576040775f44
Addison Spencer <Addison_spencer@outlook.com>
2022-03-29 18:05:12 +1100

    added formatting

--- script.js
+++ script.js
@@ -18,81 +18,80 @@ var slide = false
 
 function gravity() {
 
-     if (squarey < 200){
+    if (squarey < 200) {
         yvel += grav
         squarey += yvel
-     }else{
+    } else {
         yvel = 0
 
-     };
-        
+    };
+
 }
 
 
-addEventListener("keydown", event =>{
+addEventListener("keydown", event => {
     keys[event.key] = true;
 });
-addEventListener("keyup", event =>{
+addEventListener("keyup", event => {
     delete keys[event.key];
 });
 
 function render(mod, slided) {
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "red";
-  if (slide == false){
-    ctx.fillRect(squarex, squarey, 10, 25);
-}
-if (slide == true){
-    ctx.fillRect(squarex, squarey+10, 10, 15);
-}
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "red";
+    if (slide == false) {
+        ctx.fillRect(squarex, squarey, 10, 25);
+    }
+    if (slide == true) {
+        ctx.fillRect(squarex, squarey + 10, 10, 15);
+    }
 
 
 }
 
-function update(mod) {  // for physics 
+function update(mod) { // for physics 
     if (keys.a) {
-        squarex -= speed*mod
+        squarex -= speed * mod
     }
     if (keys.d) {
-        squarex -= 0-(speed*mod)
+        squarex -= 0 - (speed * mod)
     }
     if (keys.w) {
-        if (squarey + yvel > 199){
-            yvel -= 700*mod
+        if (squarey + yvel > 199) {
+            yvel -= 700 * mod
         }
     }
-    if (keys.s && slided < 30*mod) {
+    if (keys.s && slided < 30 * mod) {
         slide = true
-        slides -= 1500*mod
-        slided += 1*mod
-        if (keys.s != true){
-        }
+        slides -= 1500 * mod
+        slided += 1 * mod
+        if (keys.s != true) {}
         console.log("crouch")
         if (keys.a) {
-            squarex -= slides*mod
+            squarex -= slides * mod
         }
         if (keys.d) {
-            squarex -= 0-(slides*mod)
+            squarex -= 0 - (slides * mod)
         }
-    }else{
+    } else {
         slide = false
     }
-    if (keys.s != true){
+    if (keys.s != true) {
         slides = 700
         slided = 0
     }
-    console.log(slided*mod)
-
-    
+    console.log(slided * mod)
 
 
-   
-     yvel += grav
-    if (squarey + yvel > 200){
+
+
+
+    yvel += grav
+    if (squarey + yvel > 200) {
         yvel = 0
-    }else{
- 
+    } else {
+
     }
 
 
@@ -107,10 +106,10 @@ function update(mod) {  // for physics
 
 
 function main() {
-  update((Date.now() - then) / 1000);
-  render();
-  then = Date.now()
-  requestAnimationFrame(main);
+    update((Date.now() - then) / 1000);
+    render();
+    then = Date.now()
+    requestAnimationFrame(main);
 }
 var then = Date.now();
 main();
